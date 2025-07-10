7c50b4f697bef0ab29818995c667a0aba0e490fc
gaddiellr <112184077+gaddiellr@users.noreply.github.com>
2025-07-10 14:28:27 -0600

    Update activity.h

--- Complexity/activity.h
+++ Complexity/activity.h
@@ -1,41 +1,41 @@
-// Funciones de sumatoria de 1 hasta n
-// Gaddiel Lara Roldán A01704231
+// Summation from 1 to n functions
+// Gaddiel Lara Roldán
 // 27/08/2022
 
 #ifndef ACTIVITY_H
 #define ACTIVITY_H
 
-// función: sumatoria de 1 hasta n con un metodo iterativo
-// parámetro: un entero positivo n
-// salida: la sumatoria de 1 hasta n
-unsigned int sumaIterativa ( unsigned int n ) {      
-	int suma = 0 ;                                    //    O(1)
-	for (int i = 1 ; i <= n ; i++ ) {                 //    O(n)
-		suma += i ;
-	}
-	return suma ;                                     //    O(1)
+// Function: summation from 1 to n using an iterative method
+// Parameter: a positive integer n
+// Output: the summation from 1 to n
+unsigned int iterativeSum(unsigned int n) {
+    int sum = 0;                                     //    O(1)
+    for (int i = 1; i <= n; i++) {                   //    O(n)
+        sum += i;
+    }
+    return sum;                                      //    O(1)
 }
-// Complejidad: O(1)+O(n)+O(1) = O(n)+O(2)
+// Complexity: O(1) + O(n) + O(1) = O(n) + O(2)
 
 
-// función: sumatoria de 1 hasta n con un metodo recursivo
-// parámetro: un entero positivo n
-// salida: la sumatoria de 1 hasta n
-unsigned int sumaRecursiva ( unsigned int n ) {
-	if ( n == 0 ) {                                   //    O(1)
-		return 0 ;                         
-	}
-	else return n + sumaRecursiva ( n - 1 ) ;         //    O(1)
+// Function: summation from 1 to n using a recursive method
+// Parameter: a positive integer n
+// Output: the summation from 1 to n
+unsigned int recursiveSum(unsigned int n) {
+    if (n == 0) {                                    //    O(1)
+        return 0;
+    }
+    else return n + recursiveSum(n - 1);             //    O(1)
 }
-// Complejidad: O(1)+O(1) = O(2)
+// Complexity: O(1) + O(1) = O(2)
 
 
-// función: sumatoria de 1 hasta n con un metodo matemático directo
-// parámetro: un entero positivo n
-// salida: la sumatoria de 1 hasta n
-unsigned int sumaDirecta ( unsigned int n ) {        
-	return n * ( n + 1 ) / 2 ;                        //    O(1)
+// Function: summation from 1 to n using a direct mathematical method
+// Parameter: a positive integer n
+// Output: the summation from 1 to n
+unsigned int directSum(unsigned int n) {
+    return n * (n + 1) / 2;                          //    O(1)
 }
-// Complejidad: O(1)
+// Complexity: O(1)
 
 #endif /* ACTIVITY_H */
