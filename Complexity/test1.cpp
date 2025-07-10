19674b810026587d9891fcece41930d657018b0f
gaddiellr <112184077+gaddiellr@users.noreply.github.com>
2025-07-10 14:30:50 -0600

    Update test1.cpp

--- Complexity/test1.cpp
+++ Complexity/test1.cpp
@@ -14,26 +14,26 @@
 #include "catch.h"
 #include "activity.h"
 
-TEST_CASE( "Testing sumaIterativa", "[sumaIterativa]" ) {
-	REQUIRE(sumaIterativa(1) == 1);
-	REQUIRE(sumaIterativa(10) == 55);
-	REQUIRE(sumaIterativa(50) == 1275);
-	REQUIRE(sumaIterativa(100) == 5050);
-	REQUIRE(sumaIterativa(500) == 125250);
+TEST_CASE( "Testing iterativeSum", "[iterativeSum]" ) {
+	REQUIRE(iterativeSum(1) == 1);
+	REQUIRE(iterativeSum(10) == 55);
+	REQUIRE(iterativeSum(50) == 1275);
+	REQUIRE(iterativeSum(100) == 5050);
+	REQUIRE(iterativeSum(500) == 125250);
 }
 
-TEST_CASE( "Testing sumaRecursiva", "[sumaRecursiva]" ) {
-	REQUIRE(sumaRecursiva(1) == 1);
-	REQUIRE(sumaRecursiva(10) == 55);
-	REQUIRE(sumaRecursiva(50) == 1275);
-	REQUIRE(sumaRecursiva(100) == 5050);
-	REQUIRE(sumaRecursiva(500) == 125250);
+TEST_CASE( "Testing recursiveSum", "[recursiveSum]" ) {
+	REQUIRE(recursiveSum(1) == 1);
+	REQUIRE(recursiveSum(10) == 55);
+	REQUIRE(recursiveSum(50) == 1275);
+	REQUIRE(recursiveSum(100) == 5050);
+	REQUIRE(recursiveSum(500) == 125250);
 }
 
-TEST_CASE( "Testing sumaDirecta", "[sumaDirecta]" ) {
-	REQUIRE(sumaDirecta(1) == 1);
-	REQUIRE(sumaDirecta(10) == 55);
-	REQUIRE(sumaDirecta(50) == 1275);
-	REQUIRE(sumaDirecta(100) == 5050);
-	REQUIRE(sumaDirecta(500) == 125250);
+TEST_CASE( "Testing directSum", "[directSum]" ) {
+	REQUIRE(directSum(1) == 1);
+	REQUIRE(directSum(10) == 55);
+	REQUIRE(directSum(50) == 1275);
+	REQUIRE(directSum(100) == 5050);
+	REQUIRE(directSum(500) == 125250);
 }
